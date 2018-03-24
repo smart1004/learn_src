acd7b9607a1ac54dc0d9026739c23f4cc8b611a9
smart1004 <kusung25@naver.com>
2018-03-24 12:57:56 +0900

    add

--- lstm_tensorflow/rnn_words.py
+++ lstm_tensorflow/rnn_words.py
@@ -1,4 +1,6 @@
 '''
+https://towardsdatascience.com/lstm-by-example-using-tensorflow-feb0c1968537
+
 A Recurrent Neural Network (LSTM) implementation example using TensorFlow..
 Next word prediction after n_input words learned from text file.
 A story is automatically generated if the predicted word is fed back as input.
